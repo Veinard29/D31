d70f33494fced9dcc2126ae14204e6f87e373cae
Veinard29 <vlad73529@mail.ru>
2015-03-22 11:48:27 +0300

    Update VKStudentList.cpp

--- VKStudentList.cpp
+++ VKStudentList.cpp
@@ -1,74 +1,50 @@
 #include "VKStudentList.h"
-VKStudentList *VKStudentList::GetExcellentStudents(void)
+string VKStudentList::GetClassName()
 {
-  VKStudentList *e = new VKStudentList();
-
-  if (head == 0)
-    return e;
-  while (head->GetPreviousObject() != 0)
-    head = head->GetPreviousObject();
-
-  VKListObject *tmp = head;
-  while (tmp != 0)
+  return "VKStudentList";
+}
+VKStudentList* VKStudentList::GetExcellentStudents()
+{
+  VKStudentList* e = new VKStudentList();
+  for (list<VKStudent>::iterator s = students.begin(); s != students.end(); ++s)
   {
-    VKStudent *s = dynamic_cast<VKStudent*>(tmp->GetObject());
     if (s->IsExcellent())
     {
-      e->Add(new VKStudent(s->GetName(), s->GetSex(), s->GetAge()));
+      e->students.push_back(VKStudent(s->GetName(), s->GetSex(), s->GetAge()));
     }
-    tmp = tmp->GetNextObject();
   }
   return e;
 }
-VKStudentList *VKStudentList::GetBadStudents(void)
+VKStudentList* VKStudentList::GetBadStudents()
 {
-  VKStudentList *e = new VKStudentList();
-
-  if (head == 0)
-    return e;
-  while (head->GetPreviousObject() != 0)
-    head = head->GetPreviousObject();
-  VKListObject *tmp = head;
-  while (tmp != 0)
+  VKStudentList* e = new VKStudentList();
+  for (list<VKStudent>::iterator s = students.begin(); s != students.end(); ++s)
   {
-    VKStudent *s = dynamic_cast<VKStudent*>(tmp->GetObject());
     if (s->IsBad())
     {
-      e->Add(new VKStudent(s->GetName(), s->GetSex(), s->GetAge()));
+      e->students.push_back(VKStudent(s->GetName(), s->GetSex(), s->GetAge()));
     }
-    tmp = tmp->GetNextObject();
   }
   return e;
 }
-float VKStudentList::Average(void)
+float VKStudentList::Average()
 {
   float avg = 0.0;
   unsigned int count = 0;
-  if (head == 0)
-    return avg;
-  while (head->GetPreviousObject() != 0)
-    head = head->GetPreviousObject();
-  VKListObject *tmp = head;
-  while (tmp != 0)
+  for (list<VKStudent>::iterator s = students.begin(); s != students.end(); ++s)
   {
-    avg += dynamic_cast<VKStudent*>(tmp->GetObject())->GetMarks()->Average();
+    avg += s->GetMarks()->Average();
     count++;
-    tmp = tmp->GetNextObject();
   }
   return avg / count;
 }
-float VKStudentList::Average(VKSubject &subject)
+float VKStudentList::Average(const VKSubject& subject)
 {
   float avg = 0.0;
   unsigned int count = 0;
-  if (head == 0)
-    return avg;
-  while (head->GetPreviousObject() != 0)
-    head = head->GetPreviousObject();
-  VKListObject *tmp = head;
-  while (tmp != 0)
+  for (list<VKStudent>::iterator s = students.begin(); s != students.end(); ++s)
   {
-    float a = dynamic_cast<VKStudent*>(tmp->GetObject())->GetMarks()->Average(subject);
+    float a = s->GetMarks()->Average(subject);
     if (a > 0)
     {
       avg += a;
@@ -76,4 +52,14 @@ float VKStudentList::Average(VKSubject &subject)
     }
   }
   return avg / count;
+}
+
+string VKStudentList::Print()
+{
+  stringstream result;
+  for (list<VKStudent>::iterator s = students.begin(); s != students.end(); ++s)
+  {
+    result << s->Print() << endl;
+  }
+  return result.str();
 }

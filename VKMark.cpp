462f54b9fd8fa232e66f9582793704406dd262f1
Veinard29 <vlad73529@mail.ru>
2015-03-22 11:40:01 +0300

    Update VKMark.cpp

--- VKMark.cpp
+++ VKMark.cpp
@@ -1,42 +1,42 @@
 #include "VKMark.h"
-VKMark::VKMark(void)
+VKMark::VKMark()
 {
   subject = VKSubject();
   mark = 0;
 }
-VKMark::VKMark(VKSubject _subject, unsigned int _mark)
+VKMark::VKMark(const VKSubject& _subject, const unsigned int& _mark)
 {
   subject = _subject;
   mark = _mark;
 }
-string VKMark::ClassName(void)
+string VKMark::GetClassName()
 {
   return "VKMark";
 }
-string VKMark::Print(void)
+string VKMark::Print()
 {
   stringstream ss;
   ss << "Subject: " << subject.GetSubjectName() << " Mark: " << mark;
   return ss.str();
 }
-bool VKMark::operator==(VKObject &object)
+bool VKMark::operator==(const VKMark& object)
 {
-  VKMark &m = dynamic_cast<VKMark&>(object);
+  VKMark m = object;
   return (m.GetSubject() == GetSubject() && m.GetMark() == GetMark());
 }
-VKSubject VKMark::GetSubject(void)
+VKSubject VKMark::GetSubject()
 {
   return subject;
 }
-void VKMark::SetSubject(VKSubject _subject)
+void VKMark::SetSubject(const VKSubject& _subject)
 {
   subject = _subject;
 }
-unsigned int VKMark::GetMark(void)
+unsigned int VKMark::GetMark()
 {
   return mark;
 }
-void VKMark::SetMark(unsigned int _mark)
+void VKMark::SetMark(const unsigned int& _mark)
 {
   mark = _mark;
 }

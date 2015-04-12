0a77d130292de1f2743e5bf77e8927123396567a
Veinard29 <vlad73529@mail.ru>
2015-04-12 13:15:12 +0300

    Update VKMarkList.h

--- VKMarkList.h
+++ VKMarkList.h
@@ -1,22 +1,16 @@
-#ifndef _VKMARK_H_
-#define _VKMARK_H_
-#include "VKObject.h"
-#include "VKSubject.h"
-class VKMark : public VKObject
+#pragma once
+
+#include "VKMark.h"
+class VKMarkList : public VKObject
 {
 public:
-  VKMark();
-  VKMark(const VKSubject& _subject, const unsigned int& _mark);
   string GetClassName();
+  unsigned int MinMark();
+  unsigned int MaxMark();
+  float Average();
+  float Average(const VKSubject& subject);
   string Print();
-  bool operator==(const VKMark& object);
-  VKSubject GetSubject();
-  void SetSubject(const VKSubject& _subject);
-  unsigned int GetMark();
-  void SetMark(const unsigned int& _mark);
+  void Add(const VKMark& mark);
 private:
-  VKSubject subject;
-  unsigned int mark;
+  list <VKMark> marks;
 };
-
-#endif

b7724ba744c40648ebb0fb7d8ecd929ef2e72059
Veinard29 <vlad73529@mail.ru>
2015-04-12 13:22:20 +0300

    Update VKStudent.h

--- VKStudent.h
+++ VKStudent.h
@@ -1,5 +1,5 @@
-#ifndef _VKSTUDENT_H_
-#define _VKSTUDENT_H_
+#pragma once
+
 #include "VKObject.h"
 #include "VKMarkList.h"
 class VKStudent : public VKObject
@@ -8,24 +8,19 @@ public:
   VKStudent();
   VKStudent(const string& _Name, const bool& _Sex, const unsigned int& _Age, VKMarkList* _marks = 0);
   ~VKStudent();
+
   string GetClassName();
   string Print();
   bool operator==(const VKStudent& object);
+
   string GetName();
   void SetName(const string& _Name);
   bool GetSex();
   void SetSex(const bool& _Sex);
   unsigned int GetAge();
   void SetAge(const unsigned int& _Age);
-  VKMarkList* GetMarks();
+  VKMarkList& GetMarks();
+
   bool IsExcellent();
   bool IsBad();
   void AddMark(VKMark* mark);
-private:
-  string Name;
-  bool Sex;
-  unsigned int Age;
-  VKMarkList* marks;
-};
-
-#endif  

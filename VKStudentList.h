a4feb20f3561156574d6607aae101d940609282e
Veinard29 <vlad73529@mail.ru>
2015-04-12 13:24:37 +0300

    Update VKStudentList.h

--- VKStudentList.h
+++ VKStudentList.h
@@ -1,17 +1,19 @@
-#ifndef _VKSTUDENTLIST_H_
-#define _VKSRUDENTLIST_H_
+pragma once
+
 #include "VKSubject.h"
 #include "VKStudent.h"
 class VKStudentList : public VKObject
 {
 public:
   string GetClassName();
-  VKStudentList* GetExcellentStudents();
-  VKStudentList* GetBadStudents();
+  VKStudentList GetExcellentStudents();
+  VKStudentList GetBadStudents();
   float Average();
   float Average(const VKSubject& subject);
   string Print();
+  void Clear();
+  void Add(const VKStudent& student);
+  void Remove(const VKStudent& student);
+private:
   list <VKStudent> students;
 };
-
-#endif

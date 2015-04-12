9c774138bd01d4501fd1c04b1820b51ee8ffb87d
Veinard29 <vlad73529@mail.ru>
2015-04-12 13:26:32 +0300

    Update VKSubject.h

--- VKSubject.h
+++ VKSubject.h
@@ -1,18 +1,18 @@
-#ifndef _VKSUBJECT_H_
-#define _VKSUBJECT_H_
+#pragma once
+
 #include "VKObject.h"
 class VKSubject : public VKObject
 {
 public:
   VKSubject();
   VKSubject(const string& _SubjectName);
+
   string GetClassName();
   string Print();
   bool operator==(const VKSubject& object);
+
   string GetSubjectName();
   void SetSubjectName(const string& _SubjectName);
 private:
   string SubjectName;
 };
-
-#endif  

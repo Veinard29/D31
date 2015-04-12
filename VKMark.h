4c578e0ae8323117388f14a6eaf633426582d72b
Veinard29 <vlad73529@mail.ru>
2015-04-12 13:12:09 +0300

    Update VKMark.h

--- VKMark.h
+++ VKMark.h
@@ -1,5 +1,5 @@
-#ifndef _VKMARK_H_
-#define _VKMARK_H_
+#pragma once
+
 #include "VKObject.h"
 #include "VKSubject.h"
 class VKMark : public VKObject
@@ -7,16 +7,17 @@ class VKMark : public VKObject
 public:
   VKMark();
   VKMark(const VKSubject& _subject, const unsigned int& _mark);
+
   string GetClassName();
   string Print();
   bool operator==(const VKMark& object);
+
   VKSubject GetSubject();
   void SetSubject(const VKSubject& _subject);
   unsigned int GetMark();
   void SetMark(const unsigned int& _mark);
+
 private:
   VKSubject subject;
   unsigned int mark;
 };
-
-#endif

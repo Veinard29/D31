47f0cc74fd0c47946303384996384a6fb0c4f9e4
Veinard29 <vlad73529@mail.ru>
2015-04-12 13:03:49 +0300

    Update VKList.h

--- VKList.h
+++ VKList.h
@@ -1,18 +1,17 @@
-#ifndef _VKLIST_H_
-#define _VKLIST_H_
+#pragma once
 #include "VKListObject.h"
 class VKList
 {
 public:
+
   VKList(void);
   VKList(VKObject *object);
   virtual ~VKList(void);
   virtual string Print(void);
+
   virtual void Add(VKObject *object);
   virtual void Remove(VKObject *object);
   virtual void Clear(void);
 protected:
   VKListObject *head;
 };
-
-#endif  
